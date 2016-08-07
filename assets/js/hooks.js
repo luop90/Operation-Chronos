f38d934f506e61718a892ba75307b9117e08d152
Luke Henke <luop90@gmx.com>
2016-08-06 22:13:52 -0600

    Menu now opens on click

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,16 +1,20 @@
 function menuHooks() {
   var menu = document.getElementsByClassName('popupMenu')[0];
   var menuBtn = document.querySelector('aside.menu > a');
+  var canvas = document.querySelector('canvas');
 
-  menuBtn.onmouseover = function () {
-    menu.style.visibility = 'visible';
+  menuBtn.onclick = function () {
+    menu.style.visibility = (menu.style.visibility == 'hidden' || menu.style.visibility == '') ? 'visible' : 'hidden';
   }
 
-  menuBtn.onmouseout = function () {
-    menu.style.visibility = 'hidden';
+  canvas.onclick = function () {
+    if (menu.style.visibility == 'visible') {
+      menu.style.visibility = 'hidden';
+    }
   }
 }
 
 window.addEventListener('load', function () {
+  console.log('Window loaded');
   menuHooks();
 });
