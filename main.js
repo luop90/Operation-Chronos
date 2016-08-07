a8a43eb96e467423d2e61a6ce20d2f1d658ef738
Luke Henke <luop90@gmx.com>
2016-08-06 21:12:06 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+let currentWindow;
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (currentWindow == null) {
+    createWindow();
+  }
+});
+
+function createWindow() {
+  if (currentWindow) {
+    throw new Error('A window is already open');
+  }
+
+  currentWindow = new BrowserWindow({
+    autoHideMenuBar: false,
+    title: 'Operation Chronos',
+    width: 300,
+    height: 300
+  });
+}
