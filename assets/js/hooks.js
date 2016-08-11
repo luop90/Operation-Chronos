092ef339e85842bfbe6795eed8f01dd939771ba4
Luke Henke <luop90@gmx.com>
2016-08-10 19:24:15 -0600

    Canvas is now drawn on load

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -2,19 +2,22 @@ function menuHooks() {
   var menu = document.getElementsByClassName('popupMenu')[0];
   var menuBtn = document.querySelector('aside.menu > a');
   var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
 
   menuBtn.onclick = function () {
     menu.style.visibility = (menu.style.visibility == 'hidden' || menu.style.visibility == '') ? 'visible' : 'hidden';
-  }
+  };
 
   canvas.onclick = function () {
     if (menu.style.visibility == 'visible') {
       menu.style.visibility = 'hidden';
     }
-  }
+  };
+
 }
 
 window.addEventListener('load', function () {
   console.log('Window loaded');
   menuHooks();
+  Chronos.draw();
 });
