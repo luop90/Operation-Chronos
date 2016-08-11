2bbcfce7576647b4a351fcb01b972138df753819
Luke Henke <luop90@gmx.com>
2016-08-10 19:42:27 -0600

    Adjusted height / width of window

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ function createWindow() {
   currentWindow = new BrowserWindow({
     autoHideMenuBar: true,
     title: 'Operation Chronos',
-    width: 300,
-    height: 300
+    width: 270,
+    height: 295
   });
 
   currentWindow.loadURL(`file://${__dirname}/html/index.html`);
